9dbba45d59b77212a77478f0ba2d9d39d6e294a8
Daniel Bacab <dany-black88@hotmail.com>
2024-02-23 12:24:59 -0600

    Arreglado el detalle anterior, ahora todo landscape, mobile o pc, esta listo, falta vertical en mobile

--- lib/controllers/expandable_provider.dart
+++ lib/controllers/expandable_provider.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ExpandableProvider extends ChangeNotifier {
-  
+  bool isMobile = false;
+
+  changeIsMobile(bool input) {
+    isMobile = input;
+  }
+
   //Los vectores indican cuales elementos estan expandidos o no
   List sections = [
     //Seccion 0 es la seccion de experience
-    [false, false, false,false],
-    [0.0, 0.0, 0.0,0.0],
-    //Esta seccion seria la 2 en indice, pero se refiere a la de Projects
-    [false, false, false,false],
+    [false, false, false, false],
     [0.0, 0.0, 0.0, 0.0],
-    ];
+    //Esta seccion seria la 2 en indice, pero se refiere a la de Projects
+    [false, false, false, false],
+    [0.0, 0.0, 0.0, 0.0],
+  ];
 
   double currentSection1Offset = 0;
-   double currentSection2Offset = 0;
+  double currentSection2Offset = 0;
 
   void calculate1() {
-   
-    currentSection1Offset =
-        (sections[0][0] ? sections[1][0] : 0.0) +
-            (sections[0][1] ? sections[1][1] : 0.0) +
-            (sections[0][2] ? sections[1][2] : 0.0) +
-            (sections[0][3] ? sections[1][3] : 0.0);
+    currentSection1Offset = (sections[0][0] ? sections[1][0] : 0.0) +
+        (sections[0][1] ? sections[1][1] : 0.0) +
+        (sections[0][2] ? sections[1][2] : 0.0) +
+        (sections[0][3] ? sections[1][3] : 0.0);
 
-      //Estos valores de 612.0 y 609.0 son solo para pc, para mobil queda grande. Esto queda pendienteq
-     currentSection2Offset =
-        (sections[2][0] ? 612.0 : 0.0) +
-            (sections[2][1] ? 609.0 : 0.0) +
-            (sections[2][2] ? sections[3][2] : 0.0)+
-            (sections[2][3] ? sections[3][3] : 0.0);       
+    //Estos valores de 612.0 y 609.0 son solo para pc, para mobil queda grande. Esto queda pendienteq
+    currentSection2Offset = (sections[2][0] ? (isMobile?412.0:612.0) : 0.0) +
+        (sections[2][1] ? (isMobile?409.0:609.0) : 0.0) +
+        (sections[2][2] ? sections[3][2] : 0.0) +
+        (sections[2][3] ? sections[3][3] : 0.0);
 
     notifyListeners();
   }
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_interpolation_to_compose_strings
 
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:web_page/controllers/appbar_provider.dart';
@@ -91,6 +90,12 @@ class _MainPageState extends State<MainPage> {
 
     FontWeight navbarWeight = FontWeight.w800;
 
+    //Determinate if it's protraid and mobile by dimensions
+    bool isportraid = screenHeight > screenWidth;
+    bool isMobile = screenWidth < 850;
+
+    expandableProvider.changeIsMobile(isMobile);
+
     return SafeArea(
       child: WebSmoothScroll(
         controller: scrollController,
@@ -108,22 +113,74 @@ class _MainPageState extends State<MainPage> {
               child: CustomScrollView(
                 controller: scrollController,
                 slivers: <Widget>[
-
                   //APPBAR
-                  Responsive(mobile: CustomAppbarMobile(screenHeight: screenHeight, currentHeight: currentHeight, screenWidth: screenWidth, text: text, titles: titles, introductions: introductions, appbarProvider: appbarProvider, navbarWeight: navbarWeight, about: about, experience: experience, projects: projects, social: social, scrollController: scrollController), desktop: CustomAppbar(screenHeight: screenHeight, currentHeight: currentHeight, screenWidth: screenWidth, text: text, titles: titles, introductions: introductions, appbarProvider: appbarProvider, navbarWeight: navbarWeight, about: about, experience: experience, projects: projects, social: social, scrollController: scrollController))
-                  ,
+                  Responsive(
+                      mobile: CustomAppbarMobile(
+                          screenHeight: screenHeight,
+                          currentHeight: currentHeight,
+                          screenWidth: screenWidth,
+                          text: text,
+                          titles: titles,
+                          introductions: introductions,
+                          appbarProvider: appbarProvider,
+                          navbarWeight: navbarWeight,
+                          about: about,
+                          experience: experience,
+                          projects: projects,
+                          social: social,
+                          scrollController: scrollController),
+                      desktop: CustomAppbar(
+                          screenHeight: screenHeight,
+                          currentHeight: currentHeight,
+                          screenWidth: screenWidth,
+                          text: text,
+                          titles: titles,
+                          introductions: introductions,
+                          appbarProvider: appbarProvider,
+                          navbarWeight: navbarWeight,
+                          about: about,
+                          experience: experience,
+                          projects: projects,
+                          social: social,
+                          scrollController: scrollController)),
 
                   //sections 1
-                  Section1(about: about, screenHeight: screenHeight, screenWidth: screenWidth, text: text, titles: titles, introductions: introductions),
-                  
+                  Section1(
+                      about: about,
+                      screenHeight: screenHeight,
+                      screenWidth: screenWidth,
+                      text: text,
+                      titles: titles,
+                      introductions: introductions),
+
                   //section 2
-                  Section2(experience: experience, screenHeight: screenHeight, screenWidth: screenWidth, text: text, titles: titles, introductions: introductions, expandableProvider: expandableProvider),
+                  Section2(
+                      experience: experience,
+                      screenHeight: screenHeight,
+                      screenWidth: screenWidth,
+                      text: text,
+                      titles: titles,
+                      introductions: introductions,
+                      expandableProvider: expandableProvider),
 
                   //sections 3
-                  Section3(projects: projects, screenHeight: screenHeight, screenWidth: screenWidth, text: text, titles: titles, introductions: introductions, expandableProvider: expandableProvider),
+                  Section3(
+                      projects: projects,
+                      screenHeight: screenHeight,
+                      screenWidth: screenWidth,
+                      text: text,
+                      titles: titles,
+                      introductions: introductions,
+                      expandableProvider: expandableProvider),
 
                   //Social
-                  Social(social: social, screenHeight: screenHeight, screenWidth: screenWidth, text: text, titles: titles, introductions: introductions),
+                  Social(
+                      social: social,
+                      screenHeight: screenHeight,
+                      screenWidth: screenWidth,
+                      text: text,
+                      titles: titles,
+                      introductions: introductions),
 
                   //Fin
                 ],
@@ -135,7 +192,6 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
 
 _openURL(url) async {
   url = Uri.parse(url);
