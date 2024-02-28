86f58d7d1721a5d536ea7fc3a4196dfb4401e37a
Daniel Bacab <dany-black88@hotmail.com>
2024-02-27 22:16:57 -0600

    imagen y Daniel Bacab del appbar mejora de responsividad en desktop, la imagen puede mejorar si se aplica lo mismo de Daniel Bacab en terminos de posicion y usar el Center

--- lib/custom_widgets/custom_appbar.dart
+++ lib/custom_widgets/custom_appbar.dart
@@ -39,15 +39,47 @@ class CustomAppbar extends StatefulWidget {
 }
 
 class _Section1State extends State<CustomAppbar> {
-  late double profileImageWidth;
+/*   double _textWidth = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      // This callback runs after the widget is rendered on screen
+      calculateTextWidth();
+    });
+  }
+
+  void calculateTextWidth() {
+    final textSpan = TextSpan(
+        text: 'Daniel Bacab',
+        style: TextStyle(
+            fontSize: widget.currentHeight *
+                .2)); // Replace 'Hello World' with your text
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter
+        .layout(); // This will calculate the width and height of the text
+    setState(() {
+      _textWidth = textPainter.width;
+     
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant CustomAppbar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.currentHeight != widget.currentHeight) {
+      calculateTextWidth();
+       print("Tenemos: ${ widget.currentHeight}");
+    }
+  } */
+
   @override
   Widget build(BuildContext context) {
-    profileImageWidth = (widget.screenWidth / widget.screenHeight) *
-        190 *
-        (widget.currentHeight / widget.screenHeight);
-
-    print(exp(1));
-
     return SliverAppBar(
       pinned: true,
       backgroundColor: widget.appbarProvider.sliverAppBarColor,
@@ -57,52 +89,58 @@ class _Section1State extends State<CustomAppbar> {
           widget.currentHeight = constraints.maxHeight;
 
           widget.appbarProvider.currentHeight = constraints.maxHeight;
-
+          print(
+              "Tengo ${exp(8 *
+                            ((widget.currentHeight / widget.screenHeight) -
+                                1)) *
+                        (widget.screenHeight * .35)}");
           return Stack(
             children: [
               Stack(children: <Widget>[
                 Positioned(
                   left: ((widget.screenWidth / 2) -
-                          (( 0.6 *
-                            widget.currentHeight) /
-                              2)) -
-
+                          ((0.6 * widget.currentHeight) / 2)) -
                       ((1 - (widget.currentHeight / widget.screenHeight)) *
-                          (widget.screenWidth / 2))
-                  ,
-                  top: ((widget.screenHeight/2)-(( 0.6 *
-                            widget.currentHeight)/2))- ((1 - (widget.currentHeight / widget.screenHeight)) *(widget.screenHeight / 2)) ,
+                          (widget.screenWidth / 2)),
+                  top: ((widget.screenHeight / 2) -
+                          ((0.6 * widget.currentHeight) / 2)) -
+                      ((1 - (widget.currentHeight / widget.screenHeight)) *
+                          (widget.screenHeight / 2)),
                   child: ClipPath(
                       clipper: OvalClipper(),
                       child: Image.asset(
                         'images/profile.jpg',
-                        height: 
-                            0.6 *
-                            widget.currentHeight,
-                        width:  0.6 *
-                            widget.currentHeight,
+                        height: 0.6 * widget.currentHeight,
+                        width: 0.6 * widget.currentHeight,
                         fit: BoxFit.cover,
                       )),
                 ),
               ]),
               Stack(children: <Widget>[
                 Positioned(
-                    left: (widget.screenWidth * .06) +
+                    left: -((1 - (widget.currentHeight / widget.screenHeight)) *
+                        (widget.screenWidth /
+                            2.25)) /* (widget.screenWidth * .06) +
                         widget.screenWidth *
                             .3 *
-                            (widget.currentHeight / widget.screenHeight),
-                    top: (-2.5 * (widget.screenHeight / widget.currentHeight)) +
-                        widget.screenHeight *
-                            .75 *
-                            (widget.currentHeight / widget.screenHeight),
-                    child: Text(
-                      'Daniel Bacab',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: (widget.screenWidth / widget.screenHeight) *
-                                  (widget.currentHeight / widget.screenHeight) *
-                                  25 +
-                              (.025 * widget.screenHeight)),
+                            (widget.currentHeight / widget.screenHeight) */
+                    ,
+                    top: exp(8 *
+                            ((widget.currentHeight / widget.screenHeight) -
+                                1)) *
+                        (widget.screenHeight * .35) ,
+                    child: SizedBox(
+                      width: widget.screenWidth,
+                      height: widget.currentHeight,
+                      child: Center(
+                        child: Text(
+                          'Daniel Bacab',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: widget.currentHeight * .05 +
+                                  widget.screenHeight * .02),
+                        ),
+                      ),
                     ))
               ]),
               Stack(children: <Widget>[
@@ -111,10 +149,10 @@ class _Section1State extends State<CustomAppbar> {
                         widget.screenWidth *
                             .3 *
                             (widget.currentHeight / widget.screenHeight),
-                    top: (-2.5 * (widget.screenHeight / widget.currentHeight)) +
+                    top:  (-2.5 * (widget.screenHeight / widget.currentHeight)) +
                         widget.screenHeight *
                             .87 *
-                            (widget.currentHeight / widget.screenHeight),
+                            (widget.currentHeight / widget.screenHeight) ,
                     child: Text(
                       widget.appbarProvider.isExpanded
                           ? 'Flutter Developer'
